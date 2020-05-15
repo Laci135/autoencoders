7ee12efcc7d09731ca1b1d25ae75e7ff9df85deb
Laci135 <47644893+Laci135@users.noreply.github.com>
2020-05-16 01:30:14 +0200

    update composite.py add comments

--- modules/composite.py
+++ modules/composite.py
@@ -1,14 +1,17 @@
 import modules
 
+# abstract class for composite modules (may contain other modules)
 class Composite(modules.Module):
     
     def __init__(self):
         super(Composite, self).__init__()
-        self.__components = {}
-
+        self.__components = {} # dict of components
+    
+    # add a named component
     def add(self, name, component):
         self.__components[name] = component
 
+    # get a component by name
     def get(self, name):
         assert name in self.__components, f"Component not found: {name}"
         return self.__components[name]
