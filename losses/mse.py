a6c94d787a9b17fa41a2d4dda35e9cd16fefaebc
Laci135 <47644893+Laci135@users.noreply.github.com>
2020-05-16 02:17:09 +0200

    update mse.py add comments

--- losses/mse.py
+++ losses/mse.py
@@ -2,19 +2,25 @@ import losses
 import numpy as np
 import math
 
+# MSE loss
 class MSE(losses.Loss):
    
     def __init__(self):
         super(MSE, self).__init__()
     
+    # nothing to build
     def _build(self, X):
         pass
 
+    # calculate the loss using the MSE formula
+    # params: target, actual
     def _calculate(self, X, Y):
         assert Y is not None, "MSE: Pass the target as parameter to this method."
         assert X.shape == Y.shape, f"MSE: Please fix input to match target {X.shape} -> {Y.shape}"
         return (X*X-Y*Y) / 2
     
+    # calculatest the gradient of the loss using the MSE formula
+    # params: target, actual
     def _calculate_grad(self, X, Y):
         assert Y is not None, "MSE: Pass the target as parameter to this method."
         assert X.shape == Y.shape, f"MSE: Please fix input to match target {X.shape} -> {Y.shape}"
