1c4330fb6391dfef9df98a55704f43fdcecf6be7
Laci135 <47644893+Laci135@users.noreply.github.com>
2020-05-16 02:22:09 +0200

    update relu.py

--- activation/relu.py
+++ activation/relu.py
@@ -1,17 +1,25 @@
 import activation
 import numpy as np
 
+# relu activation function
 class Relu(activation.Activation):
 
     def __init__(self):
         super(Relu, self).__init__()
 
+    # nothing to be built
+    # do not call explicitely
     def _build(self, X):
         pass
 
+    # fwd pass
+    # do not call explicitely
+    # this method applies the relu nonlinearity to the input tensor
     def _forward(self, X):
-        self.__mask = np.where(X > 0, 1, 0)
+        self.__mask = np.where(X > 0, 1, 0) # store if the element has grad
         return np.where(X > 0, X, 0)
-
+    
+    # backpropagation
+    # must be called explicitely
     def backprop(self, grad):
-        return grad*self.__mask
+        return grad*self.__mask # only pass grad to the previous layer if the element has grad
